3c0df6dfbc492fa83efd3134ae6bac667e0be961
marcorogo13 <marcorogo13@gmail.com>
2023-03-08 22:46:01 +0100

    tried to fix the ALU, failing

--- lab1/alu.vhd
+++ lab1/alu.vhd
@@ -14,7 +14,9 @@ entity ALU is
 end ALU;
 
 architecture BEHAVIOR of ALU is
+
 begin
+
 
 P_ALU: process (FUNC, DATA1, DATA2)
   -- complete all the requested functions
@@ -23,16 +25,16 @@ P_ALU: process (FUNC, DATA1, DATA2)
     case FUNC is
       when ADD 	=> OUTALU <= std_logic_vector(unsigned(DATA1) + unsigned(DATA2)); 
       when SUB 	=> OUTALU <= std_logic_vector(unsigned(DATA1) - unsigned(DATA2));
-      when MULT 	=> OUTALU <= std_logic_vector(resize(unsigned (HALFD1), N) * resize(unsigned (HALFD2), N));
+      when MULT 	=> OUTALU <= std_logic_vector(unsigned((N/2 -1 downto 0 => '0') & DATA1(N/2 -1 downto 0)) * unsigned((N/2 -1 downto 0 => '0') & DATA1(N/2 -1 downto 0))); 
       when BITAND 	=> OUTALU <= DATA1 and DATA2; -- bitwise operations
       when BITOR 	=> OUTALU <= DATA1 or DATA2;
       when BITXOR 	=> OUTALU <= DATA1 xor DATA2 ;
       when FUNCLSL 	=> OUTALU  <= std_logic_vector(shift_left(unsigned(DATA1), to_integer(unsigned(DATA2)))); -- logical shift left, HELP: use the concatenation operator &  
       --DATA1(N-1-to_integer(unsigned(DATA2)) downto 0) & (to_integer(unsigned(DATA2)) - 1 downto 0 => '0'); 
       --std_logic_vector(shift_left(unsigned(DATA1), to_integer(unsigned(DATA2)))); -- logical shift left, HELP: use the concatenation operator &  
---      when FUNCLSR 	=> OUTALU <= shift_right(DATA1, to_integer(unsigned(DATA2))); -- logical shift right
---      when FUNCRL 	=> OUTALU <= rotate_left(DATA1, to_integer(unsigned(DATA2))); -- rotate left
---      when FUNCRR 	=> OUTALU <= rotate_right(DATA1, to_integer(unsigned(DATA2))); -- toate right
+      when FUNCLSR 	=> OUTALU <= std_logic_vector(shift_right(unsigned(DATA1), to_integer(unsigned(DATA2)))); -- logical shift right
+      when FUNCRL 	=> OUTALU <= std_logic_vector(rotate_left(unsigned(DATA1), to_integer(unsigned(DATA2)))); -- rotate left
+      when FUNCRR 	=> OUTALU <= std_logic_vector(rotate_right(unsigned(DATA1), to_integer(unsigned(DATA2)))); -- toate right
       when others => null;
     end case; 
   end process P_ALU;
